5487fac2137c77dd5345e668a13ff9792f2e8bd1
Souma Paul <soumapaul@icloud.com>
2025-08-09 00:05:29 +0530

    Added Counters Utilities

---
+++ Utilities/counters.py
@@ -0,0 +1,21 @@
+from tinydb import TinyDB, Query
+
+class Counters(object):
+
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        self.db = self.folder_path + "/counter_db.json"
+
+    def get_count(self, counter_name):
+        db = TinyDB(self.db)
+        Counter = Query()
+
+        record = db.get(Counter.name == counter_name)
+        value = 0
+        if record:
+            value = record['value'] + 1
+            db.update({'value': value}, Counter.name == counter_name)
+        else:
+            value = 1
+            db.insert({'name': counter_name, 'value': value})
+        return value
